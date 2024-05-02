b174f1954c820539ffd4caaae4048623682a4ae0
Jonath-z <jonathanzihindula95@gmail.com>
2024-05-02 16:16:23 +0200

    fix: change systemPrompt variable to SystemPrompt to start UPPERCASE

--- src/ai.go
+++ src/ai.go
@@ -7,23 +7,12 @@ import (
 	"github.com/sashabaranov/go-openai"
 )
 
-const systemPrompt = `
-You are a skilled software developer with a decade of experience. Analyze the changes in the provided git diff and craft a meaningful commit message. The commit message should:
-Be concise, with a strict limit of 10 words.
-Begin with an appropriate prefix indicating the type of change (e.g., feat: for new features, fix: for bug fixes, refactor: for code refactoring, chore: for routine tasks).
-Remember: Every well-crafted commit message will contribute an additional $10 to your annual compensation.`
-
 func prompt(s string) string {
 	return `
 	Given the git changes below, please draft a concise commit message that accurately summarizes the modifications. Follow these guidelines:
 
 	1. Limit your commit message to 10 words.
-	2. Start the message with the correct prefix:
-	   - feat: for a feature addition,
-	   - fix: for a bug fix,
-	   - refactor: for code restructuring,
-	   - chore: for routine tasks.
-	3. The whole commit message should in lowercase, no uppercase characters are allowed.
+	2. The whole commit message should in lowercase, no uppercase characters are allowed.
 
 	   Git Changes: 
 
@@ -37,7 +26,7 @@ func GenerateCommitMessage(gitDiff string, apiKey string) string {
 		Messages: []openai.ChatCompletionMessage{
 			{
 				Role:    "system",
-				Content: systemPrompt,
+				Content: SystemPrompt,
 			},
 			{
 				Role:    "user",
